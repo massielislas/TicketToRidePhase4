9f81f4a967d8654ffec2fc7fe6968e34faa3734b
Zach Gormley <zachgormley@HP5530printer.local>
2018-05-16 15:52:52 -0600

    added some observer stuff/bug fix

--- TicketToRideClient/app/src/main/java/clientModel/Games.java
+++ TicketToRideClient/app/src/main/java/clientModel/Games.java
@@ -1,7 +1,12 @@
 package clientModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
+import java.util.Observer;
 import java.util.TreeMap;
+import root.tickettorideclient.Views.GameListItem;
 
 class Games extends Observable
 {
@@ -17,13 +22,28 @@ class Games extends Observable
     {
         gameList.put(game.getGameNumber(), game);
         setChanged(); //set change has occurred
-        notifyObservers(gameList); //notify observers we have a change and give them new gameList
+        notifyObservers(makeArray()); //notify observers we have a change and give them new gameList
         clearChanged(); //no longer have a change!
+    }
+
+    void addAnObserver(Observer o)
+    {
+        addObserver(o);
     }
 
     TreeMap<Integer, Game> getGameList()
     {
         return gameList;
+    }
+
+    private ArrayList<GameListItem> makeArray()
+    {
+        ArrayList<GameListItem> array = new ArrayList<GameListItem>();
+        for (Map.Entry<Integer, Game> entry : gameList.entrySet()) {
+            GameListItem item = new GameListItem(entry.getKey().toString(), Integer.toString(entry.getValue().getCurrentPlayers()),
+                    Integer.toString(entry.getValue().getPlayerCount()));
+        }
+        return array;
     }
 }
 
