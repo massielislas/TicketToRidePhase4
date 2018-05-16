6b84f08ea08af97a4b27c150483e3ac32902303f
Madeleine Aydelotte <madeleineaydelotte@MadeleinesMBP.local>
2018-05-15 20:02:38 -0600

    trying to commit

--- TicketToRideClient/app/src/main/java/root/tickettorideclient/Presenters/IGamesView.java
+++ TicketToRideClient/app/src/main/java/root/tickettorideclient/Presenters/IGamesView.java
@@ -3,13 +3,14 @@ package root.tickettorideclient.Presenters;
 import java.util.List;
 
 import clientModel.Game;
+import root.tickettorideclient.Views.GameListItem;
 
 /**
  * Created by madeleineaydelotte on 5/14/18.
  */
 
 public interface IGamesView {
-    public boolean updateGamesList(List<Game> gameList);
+    public boolean updateGamesList(List<GameListItem> gameList);
     public boolean switchToWaitingView();
     public void popErrorToast(String message);
 }
