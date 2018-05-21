a40f1fca10baac8b2499e669893fd75eb056cd0e
Madeleine Aydelotte <m.c.aydelotte@gmail.com>
2018-05-21 17:04:49 -0600

    started new presenters and view/presenter interfaces

---
+++ TicketToRideClient/app/src/main/java/root/tickettorideclient/Views/ISetUpPresenter.java
@@ -0,0 +1,9 @@
+package root.tickettorideclient.Views;
+
+/**
+ * Created by madeleineaydelotte on 5/21/18.
+ */
+
+public interface ISetUpPresenter {
+    public void keepDestinationCards(/*DestinationCard[] cards*/);
+}
