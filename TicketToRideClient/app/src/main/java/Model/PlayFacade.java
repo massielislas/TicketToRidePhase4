e8d426e462cb4fd2df4ed98ee05978eb0f7ba1a6
clywzach <zach6106@gmail.com>
2018-06-08 17:15:13 -0600

    fixed observer stuff

--- TicketToRideClient/app/src/main/java/Model/PlayFacade.java
+++ TicketToRideClient/app/src/main/java/Model/PlayFacade.java
@@ -59,7 +59,7 @@ public class PlayFacade {
     public void addBoardObserver(Observer o)
     {
         boardData.addObserver(o);
-        setBoardData();
+        if (boardData != null) setBoardData();
     }
 
     public void addEndGameObserver(Observer o)
