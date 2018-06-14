2dbfc6dadf4c313f5713cdfd5c21ab9e44d0096b
Madeleine Aydelotte <m.c.aydelotte@gmail.com>
2018-06-14 15:55:09 -0600

    stubs for abstract factory / DAO

---
+++ TicketToRideServer/serverlibrary/src/main/java/DataPersistence/IDAOFactory.java
@@ -0,0 +1,10 @@
+package DataPersistence;
+
+/**
+ * Created by madeleineaydelotte on 6/14/18.
+ */
+
+public interface IDAOFactory {
+    public IGameDAO createGameDAO();
+    public IUserDAO createUserDAO();
+}
